75afe8d1f53746e35f72ddaebc7f4d50f3ff97a5
Ignacio Arganda-Carreras <ignacio.arganda@gmail.com>
2014-02-13 15:16:53 +0100

    add javadoc comments

--- src/main/java/inra/ijpb/morphology/geodrec/GeodesicReconstruction3DAlgo.java
+++ src/main/java/inra/ijpb/morphology/geodrec/GeodesicReconstruction3DAlgo.java
@@ -20,6 +20,15 @@ public interface GeodesicReconstruction3DAlgo {
 	 */
 	public ImageStack applyTo(ImageStack marker, ImageStack mask);
 	
+	/**
+	 * Applies the geodesic reconstruction algorithm to the input marker and
+	 * mask images, restricted by a binary mask.
+	 * 
+	 * @param marker image used to initialize the reconstruction
+	 * @param mask image used to constrain the reconstruction
+	 * @param binaryMask binary mask to restrict the region of application
+	 * @return the geodesic reconstruction of marker image constrained by mask image
+	 */
 	public ImageStack applyTo(ImageStack marker, ImageStack mask, ImageStack binaryMask );
 	
 	public int getConnectivity();
