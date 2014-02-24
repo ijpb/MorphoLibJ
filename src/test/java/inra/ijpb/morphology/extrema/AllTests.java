19fe60c911afafb39ddb97bd94bad0a5b59dbd29
dlegland <david.legland@grignon.inra.fr>
2014-02-24 16:55:16 +0100

    add missing test file

---
+++ src/test/java/inra/ijpb/morphology/extrema/AllTests.java
@@ -0,0 +1,15 @@
+package inra.ijpb.morphology.extrema;
+
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+	// generic classes
+	RegionalExtrema3DByFloodingTest.class,
+	RegionalExtremaByFloodingTest.class
+	})
+public class AllTests {
+  //nothing
+}
