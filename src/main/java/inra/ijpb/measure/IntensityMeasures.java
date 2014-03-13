b426d2bd6d1f706f724965ed5b76a71227851564
Ignacio Arganda-Carreras <ignacio.arganda@gmail.com>
2014-03-13 11:11:28 +0100

    Finish adding standard deviation, maximum and minimum measurements.

--- src/main/java/inra/ijpb/measure/IntensityMeasures.java
+++ src/main/java/inra/ijpb/measure/IntensityMeasures.java
@@ -58,7 +58,7 @@ public class IntensityMeasures {
 	}
 	
 	/**
-	 * Get mean voxel value per label
+	 * Get mean voxel values per label
 	 * 
 	 * @return result table with mean values per label
 	 */
@@ -72,7 +72,7 @@ public class IntensityMeasures {
 		for( int i=0; i<numLabels; i++ )
 		{
 			mean[ i ] = 0;
-			for( double v : objectVoxels[ i ] )
+			for( final double v : objectVoxels[ i ] )
 				mean[ i ] += v;
 			mean[ i ] /= objectVoxels[ i ].size();			
 		}
@@ -83,7 +83,113 @@ public class IntensityMeasures {
 		for (int i = 0; i < numLabels; i++) {
 			table.incrementCounter();
 			table.addLabel(Integer.toString( labels[i] ));
-			table.addValue("Surface", mean[i]);
+			table.addValue("Mean", mean[i]);
+		}
+
+		return table;
+	}
+	
+	/**
+	 * Get standard deviation of voxel values per label
+	 * 
+	 * @return result table with standard deviation values per label
+	 */
+	public ResultsTable getStdDev()
+	{
+		final int numLabels = objectVoxels.length;
+		
+		double[] mean = new double[ numLabels ];
+		double[] sd = new double[ numLabels ];
+		
+		// calculate mean voxel value per object
+		for( int i=0; i<numLabels; i++ )
+		{
+			mean[ i ] = 0;
+			for( final double v : objectVoxels[ i ] )
+				mean[ i ] += v;
+			mean[ i ] /= objectVoxels[ i ].size();			
+		}
+		
+		// calculate standard deviation
+		for( int i=0; i<numLabels; i++ )
+		{
+			sd[ i ] = 0;
+			for( final double v : objectVoxels[ i ] )
+				sd[ i ] += ( v - mean[ i ] ) * ( v - mean[ i ] );
+			sd[ i ] /= objectVoxels[ i ].size();	
+			sd[ i ] = Math.sqrt( sd[ i ] );
+		}
+		
+		// create data table
+		ResultsTable table = new ResultsTable();
+		for (int i = 0; i < numLabels; i++) {
+			table.incrementCounter();
+			table.addLabel(Integer.toString( labels[i] ));
+			table.addValue("StdDev", sd[i]);
+		}
+
+		return table;
+	}
+	
+	/**
+	 * Get maximum voxel values per label
+	 * 
+	 * @return result table with maximum values per label
+	 */
+	public ResultsTable getMax()
+	{
+		final int numLabels = objectVoxels.length;
+		
+		double[] max = new double[ numLabels ];
+		
+		// calculate maximum voxel value per object
+		for( int i=0; i<numLabels; i++ )
+		{
+			max[ i ] = Double.MIN_VALUE;
+			for( final double v : objectVoxels[ i ] )
+				if( v > max[ i ] )
+					max[ i ] = v;
+		}
+		
+		
+		// create data table
+		ResultsTable table = new ResultsTable();
+		for (int i = 0; i < numLabels; i++) {
+			table.incrementCounter();
+			table.addLabel(Integer.toString( labels[i] ));
+			table.addValue("Max", max[i]);
+		}
+
+		return table;
+	}
+	
+	/**
+	 * Get minimum voxel values per label
+	 * 
+	 * @return result table with minimum values per label
+	 */
+	public ResultsTable getMin()
+	{
+		final int numLabels = objectVoxels.length;
+		
+		double[] min = new double[ numLabels ];
+		
+		// calculate minimum voxel value per object
+		for( int i=0; i<numLabels; i++ )
+		{
+			min[ i ] = Double.MAX_VALUE;
+			for( final double v : objectVoxels[ i ] )
+				if( v < min[ i ] )
+					min[ i ] = v;
+		}
+		
+		
+		// create data table
+		ResultsTable table = new ResultsTable();
+		for (int i = 0; i < numLabels; i++) {
+			table.incrementCounter();
+			table.addLabel(Integer.toString( labels[i] ));
+			table.addValue("Min", min[i]);
 		}
 
 		return table;
