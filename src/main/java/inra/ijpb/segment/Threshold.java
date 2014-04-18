76f573aeb4b2dace4e3a8f2b5a7b817e5cee6a63
dlegland <david.legland@grignon.inra.fr>
2014-04-18 13:05:34 +0200

    start implemenation of some segmentation methods

---
+++ src/main/java/inra/ijpb/segment/Threshold.java
@@ -0,0 +1,59 @@
+/**
+ * 
+ */
+package inra.ijpb.segment;
+
+import ij.ImagePlus;
+import ij.process.ByteProcessor;
+import ij.process.ColorProcessor;
+import ij.process.ImageProcessor;
+
+/**
+ * Static methods for thresholding images.
+ * 
+ * @author David Legland
+ *
+ */
+public class Threshold
+{
+	public static final ImagePlus threshold(ImagePlus image, double lower, double upper)
+	{
+		String newName = image.getShortTitle() + "-bin";
+		ImageProcessor result = threshold(image.getProcessor(), lower, upper);
+		return new ImagePlus(newName, result);
+	}
+	
+	/**
+	 * Creates a new binary image with value 255 when input image has value 
+	 * between <code>lower</code> and <code>upper</code> values (included). 
+	 * @param image the input grayscale image
+	 * @param lower the lower threshold bound 
+	 * @param upper the upper threshold bound
+	 * @return a binary image
+	 */
+	public static final ImageProcessor threshold(ImageProcessor image, double lower, double upper)
+	{
+		if (image instanceof ColorProcessor) 
+		{
+			throw new IllegalArgumentException("Requires a gray scale image");
+		}
+		
+		int width = image.getWidth();
+		int height = image.getHeight();
+
+		ImageProcessor result = new ByteProcessor(width, height);
+		for (int y = 0; y < height; y++)
+		{
+			for (int x = 0; x < width; x++)
+			{
+				double value = image.getf(x, y);
+				if (value >= lower && value <= upper)
+					result.set(x, y, 255);
+			}
+
+		}
+
+		return result;
+	}
+
+}
