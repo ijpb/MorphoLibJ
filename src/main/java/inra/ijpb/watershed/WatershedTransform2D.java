848eb437a6de0bae35c642921eb80aa523381299
Ignacio Arganda-Carreras <ignacio.arganda@gmail.com>
2014-09-10 11:13:59 +0200

    Create method to obtain the whole flooding animation (one slice per h)

--- src/main/java/inra/ijpb/watershed/WatershedTransform2D.java
+++ src/main/java/inra/ijpb/watershed/WatershedTransform2D.java
@@ -6,6 +6,8 @@ import java.util.Collections;
 import java.util.LinkedList;
 
 import ij.IJ;
+import ij.ImagePlus;
+import ij.ImageStack;
 import ij.process.FloatProcessor;
 import ij.process.ImageProcessor;
 import inra.ijpb.data.Cursor2D;
@@ -696,5 +698,230 @@ public class WatershedTransform2D
 			
 		return list;
 	}
+
+	/**
+	 * Apply fast watersheds using flooding simulations, as described
+	 * by Soille, Pierre, and Luc M. Vincent. "Determining watersheds 
+	 * in digital pictures via flooding simulations." Lausanne-DL 
+	 * tentative. International Society for Optics and Photonics, 1990.
+	 * This implementation uses a binary mask to restrict regions of
+	 * application if the mask exists.
+	 *
+	 * @param hMin minimum grayscale level height
+	 * @param hMax maximum grayscale level height
+	 * @return 32-bit stack with the progression of labeled catchment basins (with dams)
+	 */
+	public ImagePlus getAnimation(
+			double hMin,
+			double hMax ) 
+	{
+	    final int size1 = inputImage.getWidth();
+	    final int size2 = inputImage.getHeight();
+	       	    
+	    final ImageStack animation = new ImageStack( size1, size2 );
+	    
+	    final boolean useMask = null != this.maskImage;
+	    
+	    // output labels
+	    final int[][] tabLabels = new int[ size1 ][ size2 ]; 
+	    
+	    // value INIT is assigned to each pixel of the output labels
+	    for( int i=0; i<size1; i++ )
+	    		Arrays.fill( tabLabels[i], INIT );
+	    
+	    int currentLabel = 0;
+	    
+	    boolean flag = false;	    
+	    
+	    // Make list of pixels and sort it in ascending order
+	    IJ.showStatus( "Extracting pixel values..." );
+	    if( verbose ) IJ.log("  Extracting pixel values (h_min = " + hMin + ", h_max = " + hMax + ")..." );
+	    final long t0 = System.currentTimeMillis();
+
+	    // list of original pixels values and corresponding coordinates
+	    ArrayList<PixelRecord> pixelList = extractPixelValues( inputImage, hMin, hMax );
+
+	    final long t1 = System.currentTimeMillis();		
+	    if( verbose ) IJ.log("  Extraction took " + (t1-t0) + " ms.");
+	    if( verbose ) IJ.log("  Sorting pixels by value..." );
+	    IJ.showStatus("Sorting pixels by value...");
+	    Collections.sort( pixelList );
+	    final long t2 = System.currentTimeMillis();
+	    if( verbose ) IJ.log("  Sorting took " + (t2-t1) + " ms.");
+	    
+	    IJ.log( "  Flooding..." );
+	    IJ.showStatus( "Flooding..." );
+	    final long start = System.currentTimeMillis();
+	    
+      	// Check connectivity
+       	final Neighborhood2D neigh = connectivity == 4 ? 
+       									new Neighborhood2DC4() : new Neighborhood2DC8();
+	    	    
+	    LinkedList<Cursor2D> fifo = new LinkedList<Cursor2D>();
+	      
+        // initial height
+        double h = hMin;
+        
+        // find corresponding pixel index
+        int currentIndex = 0;
+        while( h < hMin )
+        {
+        	h = pixelList.get( currentIndex ).getValue();
+        	currentIndex++;
+        }
+        
+        int heightIndex1 = currentIndex;
+        int heightIndex2 = currentIndex;
+        
+        
+	    // for h <- h_min to h_max; geodesic SKIZ of level h-1 inside level h
+	    while( currentIndex < pixelList.size() && h <= hMax )
+	    {	    	
+	    	h = pixelList.get( currentIndex ).getValue();	    	
+	    		    		    		    	
+	    	for(int pixelIndex = heightIndex1; pixelIndex < pixelList.size(); pixelIndex ++)
+	    	{
+	    		final PixelRecord PixelRecord = pixelList.get( pixelIndex );
+	    			    		
+	    		if( PixelRecord.getValue() != h )
+	    		{
+	    			// this pixel is at level h+1
+	    			heightIndex1 = pixelIndex;
+	    			break;
+	    		}
+	    			    		
+	    		final Cursor2D p = PixelRecord.getCursor();
+	    		final int i = p.getX();
+	    		final int j = p.getY();
+	    			    		
+	    		// set label to MASK
+	    		tabLabels[ i ][ j ] = MASK;
+
+	    		// read neighbor coordinates	    		
+	    		neigh.setCursor( p );
+	    		for( Cursor2D c : neigh.getNeighbors() )			       		
+	    		{       			
+	    			int u = c.getX();
+	    			int v = c.getY();
+
+	    			// initialize queue with neighbors at level h of current basins or watersheds
+	    			if ( u >= 0 && u < size1 && v >= 0 && v < size2 
+	    					&& tabLabels[ u ][ v ] >= WSHED 
+	    					&& useMask && maskImage.getf( u, v ) > 0 ) 
+	    				{
+	    					fifo.addLast( p );
+	    					tabLabels[ i ][ j ] = INQUEUE;
+	    					break;
+	    				}	    			
+	    		}// end for	    	
+	    	}// end for
+
+	    	while( fifo.isEmpty() == false )
+	    	{
+	    		// retrieve point p
+	    		final Cursor2D p = fifo.poll();	    		
+	    		final int i = p.getX();
+	    		final int j = p.getY();
+
+	    		// read neighbor coordinates
+	    		neigh.setCursor( p );
+
+	    		for( Cursor2D c : neigh.getNeighbors() )			       		
+	    		{
+	    			// labeling current point by inspecting neighbors
+	    			int u = c.getX();
+	    			int v = c.getY();
+
+	    			if ( u >= 0 && u < size1 && v >= 0 && v < size2 && useMask && maskImage.getf( u, v ) > 0 )
+	    			{
+	    				if ( tabLabels[ u ][ v ] > 0 ) // i.e. the pixel belongs to an already labeled basin
+	    				{
+	    					if ( tabLabels[ i ][ j ] == INQUEUE || (tabLabels[ i ][ j ] == WSHED && flag == true ) )
+	    					{
+	    						tabLabels[ i ][ j ] = tabLabels[ u ][ v ];
+	    					}
+	    					else if ( tabLabels[ i ][ j ] > 0 && tabLabels[ i ][ j ] != tabLabels[ u ][ v ] )
+	    					{
+	    						tabLabels[ i ][ j ] = WSHED;
+	    						flag = false;
+	    					}       					
+	    				}
+	    				else if ( tabLabels[ u ][ v ] == WSHED && tabLabels[ i ][ j ] == INQUEUE )
+	    				{
+	    					tabLabels[ i ][ j ] = WSHED;
+	    					flag = true;
+	    				}
+	    				else if ( tabLabels[ u ][ v ] == MASK )
+	    				{
+	    					tabLabels[ u ][ v ] = INQUEUE;
+	    					fifo.addLast( c );
+
+	    				}
+	    			}       			       			
+	    		}	    	
+	    	}
+
+	    	// check for new minima at level h
+	    		    	
+	    	for(int pixelIndex = heightIndex2; pixelIndex < pixelList.size(); pixelIndex ++, currentIndex++)
+	    	{
+	    		final PixelRecord PixelRecord = pixelList.get( pixelIndex );	    			    		
+	    		
+	    		if( PixelRecord.getValue() != h )
+	    		{
+	    			// this pixel is at level h+1
+	    			heightIndex2 = pixelIndex;
+	    			break;
+	    		}
+	    			    		
+	    		final Cursor2D p = PixelRecord.getCursor();
+	    		final int i = p.getX();
+	    		final int j = p.getY();
+	    		
+	    		if ( tabLabels[ i ][ j ] == MASK ) // the pixel is inside a new minimum
+	    		{
+	    			currentLabel ++;
+	    			fifo.addLast( p );
+	    			tabLabels[ i ][ j ] = currentLabel;
+	    			
+	    			while( fifo.isEmpty() == false )
+	    	    	{
+	    				final Cursor2D p2 = fifo.poll();
+
+	    	    		// read neighbor coordinates
+	    	    		neigh.setCursor( p2 );
+
+	    	    		for( Cursor2D c : neigh.getNeighbors() ) // inspect neighbors of p2		       		
+	    	    		{       			
+	    	    			int u = c.getX();
+	    	    			int v = c.getY();
+	    	    			
+	    	    			if ( u >= 0 && u < size1 && v >= 0 && v < size2 
+	    	    					&& tabLabels[ u ][ v ] == MASK 
+	    	    					&& useMask && maskImage.getf( u, v ) > 0 )
+	    	    			{
+	    	    				fifo.addLast( c );
+	    	    				tabLabels[ u ][ v ] = currentLabel;
+	    	    			}	    	    				    	    			
+	    	    		}// end for
+	    	    	}// end while
+	    		}// end if	    		
+	    	}// end for
+	    		    	
+	    	// update animation
+	    	animation.addSlice( "h = " + h, new FloatProcessor( tabLabels ) );
+	    	
+	    	IJ.showProgress( h / hMax );
+	    	
+	    }// end while (flooding)
+	    
+	    IJ.showProgress( 1.0 );
+	    
+	    final long end = System.currentTimeMillis();
+		if( verbose ) IJ.log("  Flooding took: " + (end-start) + " ms");	    	   
+	    				    
+	    return new ImagePlus( "Watershed flooding", animation );
+	}
+	
 	
 }// end class WatershedTransform2D
