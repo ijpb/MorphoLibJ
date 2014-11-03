616265097ae0444eeb252fab9777897d76ce3684
dlegland <david.legland@grignon.inra.fr>
2014-11-03 11:17:28 +0100

    add legacy LabelImages class for ensuring backward compatibility

---
+++ src/main/java/inra/ijpb/morphology/LabelImages.java
@@ -0,0 +1,9 @@
+package inra.ijpb.morphology;
+
+/**
+ * @deprecated as from 2014.11.03, replaced by class inra.ijpb.label.LabelImages
+ */
+@Deprecated
+public class LabelImages extends inra.ijpb.label.LabelImages {
+
+}
