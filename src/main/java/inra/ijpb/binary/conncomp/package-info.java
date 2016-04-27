70a4233b64117f47d7b8f52891190c1f8b971c8d
dlegland <david.legland@nantes.inra.fr>
2016-04-27 09:18:25 +0200

    update package info files

---
+++ src/main/java/inra/ijpb/binary/conncomp/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * <p>Connected components labeling algorithms for binary 2D or 3D images.</p>
+ * 
+ */
+package inra.ijpb.binary.conncomp;
+
+
--- src/main/java/inra/ijpb/morphology/attrfilt/package-info.java
+++ src/main/java/inra/ijpb/morphology/attrfilt/package-info.java
@@ -1,8 +1,6 @@
 /**
- * 
- */
-/**
- * @author dlegland
- *
+ * Attribute opening and filtering for grayscale images. 
+ * Comprises size opening (area opening, volume opening for 3D images), opening by 
+ * the length of the bounding box diagonal, and associated closing and top-hats.    
  */
 package inra.ijpb.morphology.attrfilt;
