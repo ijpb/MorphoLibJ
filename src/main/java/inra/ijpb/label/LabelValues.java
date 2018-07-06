5a7cad4c269be798c6d0865764e5ca5b3b83291c
dlegland <david.legland@inra.fr>
2018-07-06 16:46:16 +0200

    add support for 3D images

--- src/main/java/inra/ijpb/label/LabelValues.java
+++ src/main/java/inra/ijpb/label/LabelValues.java
@@ -3,7 +3,9 @@ package inra.ijpb.label;
 import java.awt.Point;
 import java.util.Map;
 
+import ij.ImageStack;
 import ij.process.ImageProcessor;
+import inra.ijpb.data.Cursor3D;
 
 /**
  * Utility methods for combining label images and intensity images.
@@ -61,6 +63,51 @@ public class LabelValues
 		return maxValue;
 	}
 	
+	/**
+	 * Computes the largest value within the <code>valueImage</code>, using the
+	 * strictly positive values in <code>labelImage</code> as mask. Can be used
+	 * to setup min/max display value after computing a distance transform.
+	 * 
+	 * @param valueImage
+	 *            the image containing values (e.g. a distance map)
+	 * @param labelImage
+	 *            the image containing strictly positive values for foreground
+	 * @return the maximum value within the value image restricted to the label
+	 *         image
+	 */
+	public static final double maxValueWithinLabels(ImageStack valueImage, ImageStack labelImage)
+	{
+		// get image size
+		int sizeX = valueImage.getWidth();
+		int sizeY = valueImage.getHeight();
+		int sizeZ = valueImage.getSize();
+		
+		// check image dimensions
+		if (labelImage.getWidth() != sizeX || labelImage.getHeight() != sizeY || labelImage.getSize() != sizeZ)
+		{
+			throw new IllegalArgumentException("Both images must have same dimensions");
+		}
+
+		// iterate over values
+		double maxValue = Double.NEGATIVE_INFINITY;
+		for (int z = 0; z < sizeZ; z++)
+		{
+			for (int y = 0; y < sizeY; y++)
+			{
+				for (int x = 0; x < sizeX; x++)
+				{
+					// check that current pixel is within a label
+					if (labelImage.getVoxel(x, y, z) > 0)
+					{
+						maxValue = Math.max(maxValue, valueImage.getVoxel(x, y, z));
+					}
+				}
+			}
+		}
+		return maxValue;
+	}
+	
+	
 	
 	/**
 	 * Find maximum value in intensity image for each label.
@@ -113,6 +160,72 @@ public class LabelValues
 			}
 		}
 				
+		return maxValues;
+	}
+	
+	/**
+	 * Find maximum value in intensity image for each label.
+	 * 
+	 * @param image
+	 *            the intensity 3D image
+	 * @param labelImage
+	 *            the 3D image containing labels
+	 * @param labels
+	 *            the list of labels for which the values are computed
+	 * @return an array of values corresponding to the maximum value within each
+	 *         label
+	 */
+	public static final double[] maxValues(ImageStack image,
+			ImageStack labelImage, int[] labels)
+	{
+		// get image size
+		int sizeX = labelImage.getWidth();
+		int sizeY = labelImage.getHeight();
+		int sizeZ = labelImage.getSize();
+
+		// check image dimensions
+		if (labelImage.getWidth() != sizeX || labelImage.getHeight() != sizeY || labelImage.getSize() != sizeZ)
+		{
+			throw new IllegalArgumentException("Both images must have same dimensions");
+		}
+
+		// Compute value of greatest label
+		int nLabels = labels.length;
+		
+		// init index of each label
+		Map<Integer, Integer> labelIndices = LabelImages.mapLabelIndices(labels);
+				
+		// Init value of maximum for each label
+		double[] maxValues = new double[nLabels];
+		for (int i = 0; i < nLabels; i++)
+			maxValues[i] = Double.NEGATIVE_INFINITY;
+		
+		// iterate on image pixels
+		for (int z = 0; z < sizeZ; z++) 
+		{
+			for (int y = 0; y < sizeY; y++) 
+			{
+				for (int x = 0; x < sizeX; x++) 
+				{
+					int label = (int) labelImage.getVoxel(x, y, z);
+
+					// do not process pixels that do not belong to particle
+					if (label == 0)
+						continue;
+
+					if (labelIndices.containsKey(label))
+					{
+						int index = labelIndices.get(label);
+
+						// update values and positions
+						double value = image.getVoxel(x, y, z);
+						if (value > maxValues[index])
+							maxValues[index] = value;
+					}
+				}
+			}
+		}
+		
 		return maxValues;
 	}
 	
@@ -176,6 +289,77 @@ public class LabelValues
 	}
 	
 	/**
+	 * For each label, finds the position of the 3D point belonging to label
+	 * region defined by the 3D <code>labelImage</code> and with maximal value
+	 * in intensity 3D image <code>valueImage</code>.
+	 * 
+	 * @param valueImage
+	 *            the intensity image containing values to compare
+	 * @param labelImage
+	 *            the intensity image containing label of each pixel
+	 * @param labels
+	 *            the list of labels in the label image
+	 * @return the position of maximum value in intensity image for each label
+	 */
+	public static final Position3DValuePair[] findMaxValues(ImageStack valueImage, 
+			ImageStack labelImage, int[] labels)
+	{
+		// get image size
+		int sizeX = labelImage.getWidth();
+		int sizeY = labelImage.getHeight();
+		int sizeZ = labelImage.getSize();
+
+		// check image dimensions
+		if (labelImage.getWidth() != sizeX || labelImage.getHeight() != sizeY || labelImage.getSize() != sizeZ)
+		{
+			throw new IllegalArgumentException("Both images must have same dimensions");
+		}
+
+		// Create associative map between each label and its index
+		Map<Integer,Integer> labelIndices = LabelImages.mapLabelIndices(labels);
+		
+		// Init Position and value of maximum for each label
+		int nLabels = labels.length;
+		Position3DValuePair[] pairs = new Position3DValuePair[nLabels]; 
+		for (int i = 0; i < nLabels; i++) 
+		{
+			pairs[i] = new Position3DValuePair(new Cursor3D(-1, -1, -1), Double.NEGATIVE_INFINITY);
+		}
+		
+		// iterate on image pixels
+		for (int z = 0; z < sizeZ; z++) 
+		{
+			for (int y = 0; y < sizeY; y++) 
+			{
+				for (int x = 0; x < sizeX; x++) 
+				{
+					int label = (int) labelImage.getVoxel(x, y, z);
+					
+					// do not process pixels that do not belong to any particle
+					if (label == 0)
+						continue;
+					if (!labelIndices.containsKey(label))
+						continue;
+	
+					// get position-value pair corresponding to current label
+					int index = labelIndices.get(label);
+					Position3DValuePair pair = pairs[index];
+					
+					// update values and positions
+					double value = valueImage.getVoxel(x, y, z);
+					if (value > pair.value) 
+					{
+						pair.position = new Cursor3D(x, y, z);
+						pair.value = value;
+					}
+				}
+			}
+		}
+		
+		return pairs;
+	}
+
+	/**
 	 * For each label, finds the position of the point belonging to label region
 	 * defined by <code>labelImage</code> and with minimal value in intensity
 	 * image <code>valueImage</code>.
@@ -234,6 +418,76 @@ public class LabelValues
 		return pairs;
 	}
 
+	/**
+	 * For each label, finds the position of the 3D point belonging to 3D label
+	 * region defined by <code>labelImage</code> and with minimal value in 3D
+	 * intensity image <code>valueImage</code>.
+	 * 
+	 * @param valueImage
+	 *            the intensity image containing values to compare
+	 * @param labelImage
+	 *            the intensity image containing label of each pixel
+	 * @param labels
+	 *            the list of labels in the label image
+	 * @return the position of minimum value in intensity image for each label
+	 */
+	public static final Position3DValuePair[] findMinValues(ImageStack valueImage, 
+			ImageStack labelImage, int[] labels)
+	{
+		// get image size
+		int sizeX = labelImage.getWidth();
+		int sizeY = labelImage.getHeight();
+		int sizeZ = labelImage.getSize();
+
+		// check image dimensions
+		if (labelImage.getWidth() != sizeX || labelImage.getHeight() != sizeY || labelImage.getSize() != sizeZ)
+		{
+			throw new IllegalArgumentException("Both images must have same dimensions");
+		}
+
+		// Create associative map between each label and its index
+		Map<Integer,Integer> labelIndices = LabelImages.mapLabelIndices(labels);
+		
+		// Init Position and value of maximum for each label
+		int nLabels = labels.length;
+		Position3DValuePair[] pairs = new Position3DValuePair[nLabels]; 
+		for (int i = 0; i < nLabels; i++) 
+		{
+			pairs[i] = new Position3DValuePair(new Cursor3D(-1, -1, -1), Double.POSITIVE_INFINITY);
+		}
+		
+		// iterate on image voxels
+		for (int z = 0; z < sizeZ; z++) 
+		{
+			for (int y = 0; y < sizeY; y++) 
+			{
+				for (int x = 0; x < sizeX; x++) 
+				{
+					int label = (int) labelImage.getVoxel(x, y, z);
+
+					// do not process pixels that do not belong to any particle
+					if (label == 0)
+						continue;
+					if (!labelIndices.containsKey(label))
+						continue;
+
+					// get position-value pair corresponding to current label
+					int index = labelIndices.get(label);
+					Position3DValuePair pair = pairs[index];
+
+					// update values and positions
+					double value = valueImage.getVoxel(x, y, z);
+					if (value < pair.value) 
+					{
+						pair.position = new Cursor3D(x, y, z);
+						pair.value = value;
+					}
+				}
+			}
+		}
+				
+		return pairs;
+	}
 
 	/**
 	 * For each label, finds the position of the point belonging to label region
@@ -285,6 +539,75 @@ public class LabelValues
 				{
 					posMax[index].setLocation(x, y);
 					maxValues[index] = value;
+				}
+			}
+		}
+				
+		return posMax;
+	}
+
+	/**
+	 * For each label, finds the position of the point belonging to label region
+	 * defined by <code>labelImage</code> and with maximal value in intensity
+	 * image <code>valueImage</code>.
+	 * 
+	 * @param valueImage
+	 *            the intensity image containing values to compare
+	 * @param labelImage
+	 *            the intensity image containing label of each pixel
+	 * @param labels
+	 *            the list of labels in the label image
+	 * @return the position of maximum value in intensity image for each label
+	 */
+	public static final Cursor3D[] findPositionOfMaxValues(ImageStack valueImage, 
+			ImageStack labelImage, int[] labels)
+	{
+		// get image size
+		int sizeX = labelImage.getWidth();
+		int sizeY = labelImage.getHeight();
+		int sizeZ = labelImage.getSize();
+
+		// check image dimensions
+		if (labelImage.getWidth() != sizeX || labelImage.getHeight() != sizeY || labelImage.getSize() != sizeZ)
+		{
+			throw new IllegalArgumentException("Both images must have same dimensions");
+		}
+
+		// Create associative map between each label and its index
+		Map<Integer,Integer> labelIndices = LabelImages.mapLabelIndices(labels);
+		
+		// Init Position and value of maximum for each label
+		int nLabels = labels.length;
+		Cursor3D[] posMax 	= new Cursor3D[nLabels];
+		double[] maxValues = new double[nLabels];
+		for (int i = 0; i < nLabels; i++) 
+		{
+			maxValues[i] = Double.NEGATIVE_INFINITY;
+			posMax[i] = new Cursor3D(-1, -1, -1);
+		}
+		
+		// iterate on image pixels
+		for (int z = 0; z < sizeZ; z++) 
+		{
+			for (int y = 0; y < sizeY; y++) 
+			{
+				for (int x = 0; x < sizeX; x++) 
+				{
+					int label = (int) labelImage.getVoxel(x, y, z);
+
+					// do not process pixels that do not belong to any particle
+					if (label == 0)
+						continue;
+
+					int index = labelIndices.get(label);
+
+					// update values and positions
+					double value = valueImage.getVoxel(x, y, z);
+					if (value > maxValues[index]) 
+					{
+						posMax[index] = new Cursor3D(x, y, z);
+						maxValues[index] = value;
+					}
 				}
 			}
 		}
@@ -348,6 +671,75 @@ public class LabelValues
 				
 		return posMax;
 	}
+	
+	/**
+	 * For each label, finds the position of the point belonging to label region
+	 * defined by <code>labelImage</code> and with minimal value in intensity
+	 * image <code>valueImage</code>.
+	 * 
+	 * @param valueImage
+	 *            the intensity image containing values to compare
+	 * @param labelImage
+	 *            the intensity image containing label of each pixel
+	 * @param labels
+	 *            the list of labels in the label image
+	 * @return the position of minimum value in intensity image for each label
+	 */
+	public static final Cursor3D[] findPositionOfMinValues(ImageStack valueImage, 
+			ImageStack labelImage, int[] labels)
+	{
+		// get image size
+		int sizeX = labelImage.getWidth();
+		int sizeY = labelImage.getHeight();
+		int sizeZ = labelImage.getSize();
+
+		// check image dimensions
+		if (labelImage.getWidth() != sizeX || labelImage.getHeight() != sizeY || labelImage.getSize() != sizeZ)
+		{
+			throw new IllegalArgumentException("Both images must have same dimensions");
+		}
+
+		// Create associative map between each label and its index
+		Map<Integer,Integer> labelIndices = LabelImages.mapLabelIndices(labels);
+		
+		// Init Position and value of maximum for each label
+		int nLabels = labels.length;
+		Cursor3D[] posMax 	= new Cursor3D[nLabels];
+		double[] maxValues = new double[nLabels];
+		for (int i = 0; i < nLabels; i++) 
+		{
+			maxValues[i] = Double.POSITIVE_INFINITY;
+			posMax[i] = new Cursor3D(-1, -1, -1);
+		}
+		
+		// iterate on image pixels
+		for (int z = 0; z < sizeZ; z++) 
+		{
+			for (int y = 0; y < sizeY; y++) 
+			{
+				for (int x = 0; x < sizeX; x++) 
+				{
+					int label = (int) labelImage.getVoxel(x, y, z);
+
+					// do not process pixels that do not belong to any particle
+					if (label == 0)
+						continue;
+
+					int index = labelIndices.get(label);
+
+					// update values and positions
+					double value = valueImage.getVoxel(x, y, z);
+					if (value < maxValues[index]) 
+					{
+						posMax[index] = new Cursor3D(x, y, z);
+						maxValues[index] = value;
+					}
+				}
+			}
+		}
+				
+		return posMax;
+	}
 
 	public static class PositionValuePair
 	{
@@ -361,6 +753,28 @@ public class LabelValues
 		}
 		
 		public Point getPosition()
+		{
+			return position;
+		}
+		
+		public double getValue()
+		{
+			return value;
+		}
+	}
+
+	public static class Position3DValuePair
+	{
+		Cursor3D position;
+		double value;
+		
+		public Position3DValuePair(Cursor3D position, double value)
+		{
+			this.position = position;
+			this.value = value;
+		}
+		
+		public Cursor3D getPosition()
 		{
 			return position;
 		}
