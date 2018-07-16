858e7a0b2524b6653c68a88312be8c8ff670155d
Ignacio Arganda-Carreras <ignacio.arganda@gmail.com>
2018-07-16 17:40:47 +0200

    Add class to test and debug Label Edition plugin.

---
+++ src/test/java/inra/ijpb/plugins/TestLabelEdition.java
@@ -0,0 +1,24 @@
+package inra.ijpb.plugins;
+
+import ij.IJ;
+import ij.ImageJ;
+
+/**
+ * Class to test the Label Edition plugin.
+ * @author iarganda
+ *
+ */
+public class TestLabelEdition {
+
+	/**
+	 * Main method to test and debug the Label Edition plugin
+	 *
+	 * @param args
+	 */
+	public static void main( final String[] args )
+	{
+		ImageJ.main( args );
+		IJ.open( TestMorphologicalSegmentation.class.getResource( "/files/grains-med-WTH-lbl.tif" ).getFile() );
+		new LabelEdition().run( null );
+	}
+}
