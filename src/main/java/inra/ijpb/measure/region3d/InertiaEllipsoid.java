1bae5fdb122b388c7f1c0fa95a10a4fadb0a3f11
dlegland <david.legland@inra.fr>
2018-12-03 13:47:12 +0100

    re-organize code for computing InertiaEllipsoid (split computation of inertia coefficients)

--- src/main/java/inra/ijpb/measure/region3d/InertiaEllipsoid.java
+++ src/main/java/inra/ijpb/measure/region3d/InertiaEllipsoid.java
@@ -131,183 +131,152 @@ public class InertiaEllipsoid extends RegionAnalyzer3D<Ellipsoid>
         	throw new RuntimeException("Requires the JAMA package to work properly");
         }
         
-        // size of image
-        int sizeX = image.getWidth();
-        int sizeY = image.getHeight();
-        int sizeZ = image.getSize();
-        
-		// Extract spatial calibration
-		double sx = 1, sy = 1, sz = 1;
-		double ox = 0, oy = 0, oz = 0;
-		if (calib != null)
-		{
-			sx = calib.pixelWidth;
-			sy = calib.pixelHeight;
-			sz = calib.pixelDepth;
-			ox = calib.xOrigin;
-			oy = calib.yOrigin;
-			oz = calib.zOrigin;
-		}
-		
-    	// create associative array to know index of each label
-    	HashMap<Integer, Integer> labelIndices = LabelImages.mapLabelIndices(labels);
+        // Compute 2D inertia moments for each label
+        InertiaMoments3D[] moments = computeMoments(image, labels, calib);
 
-    	// allocate memory for result
-    	int nLabels = labels.length;
-    	InertiaMoments3D[] moments = new InertiaMoments3D[nLabels]; 
-    	for (int i = 0; i < nLabels; i++)
-    	{
-    	    moments[i] = new InertiaMoments3D();
-    	}
+        // Create result array
+        int nLabels = labels.length;
+    	Ellipsoid[] ellipsoids = new Ellipsoid[nLabels];
 
-    	// compute centroid of each region
-    	fireStatusChanged(this, "Ellipsoid: compute centroids");
-    	for (int z = 0; z < sizeZ; z++) 
-    	{
-            this.fireProgressChanged(this, z, sizeZ);
-    		for (int y = 0; y < sizeY; y++)
-    		{
-    			for (int x = 0; x < sizeX; x++)
-    			{
-    				// do not process background voxels
-    				int label = (int) image.getVoxel(x, y, z);
-    				if (label == 0)
-    					continue;
-
-    				// convert label to its index
-    				if (!labelIndices.containsKey(label))
-    				{
-    				    System.err.println("Label image contains unknown label: " + label);
-    				    continue;
-    				}
-    				int index = labelIndices.get(label);
-
-    				// update sum coordinates, taking into account the spatial calibration
-    				InertiaMoments3D moment = moments[index];
-    				moment.cx += x * sx;
-    				moment.cy += y * sy;
-    				moment.cz += z * sz;
-    				moment.count++;
-    			}
-    		}
-    	}
-
-    	// normalize by number of pixels in each region
+    	// compute ellipsoid parameters for each region
+        fireStatusChanged(this, "Ellipsoid: compute SVD");
     	for (int i = 0; i < nLabels; i++) 
     	{
-            moments[i].cx /= moments[i].count;
-            moments[i].cy /= moments[i].count;
-            moments[i].cz /= moments[i].count;
+            this.fireProgressChanged(this, i, nLabels);
+            // create the new ellipsoid
+            ellipsoids[i] = moments[i].equivalentEllipsoid();
     	}
 
-    	// compute centered inertia matrix of each label
-        fireStatusChanged(this, "Ellipsoid: compute inertia matrices");
-    	for (int z = 0; z < sizeZ; z++) 
-    	{
-            this.fireProgressChanged(this, z, sizeZ);
-    		for (int y = 0; y < sizeY; y++)
-    		{
-    			for (int x = 0; x < sizeX; x++) 
-    			{
-    				// do not process background voxels
-    				int label = (int) image.getVoxel(x, y, z);
-    				if (label == 0)
-    					continue;
+    	return ellipsoids;
+	}
+	
+	public InertiaMoments3D[] computeMoments(ImageStack image, int[] labels, Calibration calib)
+	{
+	    // size of image
+	    int sizeX = image.getWidth();
+	    int sizeY = image.getHeight();
+	    int sizeZ = image.getSize();
 
-    				// convert label to its index
-    				int index = labelIndices.get(label);
+	    // Extract spatial calibration
+	    double sx = 1, sy = 1, sz = 1;
+	    double ox = 0, oy = 0, oz = 0;
+	    if (calib != null)
+	    {
+	        sx = calib.pixelWidth;
+	        sy = calib.pixelHeight;
+	        sz = calib.pixelDepth;
+	        ox = calib.xOrigin;
+	        oy = calib.yOrigin;
+	        oz = calib.zOrigin;
+	    }
+
+	    // create associative array to know index of each label
+	    HashMap<Integer, Integer> labelIndices = LabelImages.mapLabelIndices(labels);
+
+	    // allocate memory for result
+	    int nLabels = labels.length;
+	    InertiaMoments3D[] moments = new InertiaMoments3D[nLabels]; 
+	    for (int i = 0; i < nLabels; i++)
+	    {
+	        moments[i] = new InertiaMoments3D();
+	    }
+
+	    // compute centroid of each region
+	    fireStatusChanged(this, "Ellipsoid: compute centroids");
+	    for (int z = 0; z < sizeZ; z++) 
+	    {
+	        this.fireProgressChanged(this, z, sizeZ);
+	        for (int y = 0; y < sizeY; y++)
+	        {
+	            for (int x = 0; x < sizeX; x++)
+	            {
+	                // do not process background voxels
+	                int label = (int) image.getVoxel(x, y, z);
+	                if (label == 0)
+	                    continue;
+
+	                // convert label to its index
+	                if (!labelIndices.containsKey(label))
+	                {
+	                    System.err.println("Label image contains unknown label: " + label);
+	                    continue;
+	                }
+	                int index = labelIndices.get(label);
+
+	                // update sum coordinates, taking into account the spatial calibration
+	                InertiaMoments3D moment = moments[index];
+	                moment.cx += x * sx;
+	                moment.cy += y * sy;
+	                moment.cz += z * sz;
+	                moment.count++;
+	            }
+	        }
+	    }
+
+	    // normalize by number of pixels in each region
+	    for (int i = 0; i < nLabels; i++) 
+	    {
+	        moments[i].cx /= moments[i].count;
+	        moments[i].cy /= moments[i].count;
+	        moments[i].cz /= moments[i].count;
+	    }
+	    
+        // compute centered inertia matrix of each label
+        fireStatusChanged(this, "Ellipsoid: compute inertia matrices");
+        for (int z = 0; z < sizeZ; z++) 
+        {
+            this.fireProgressChanged(this, z, sizeZ);
+            for (int y = 0; y < sizeY; y++)
+            {
+                for (int x = 0; x < sizeX; x++) 
+                {
+                    // do not process background voxels
+                    int label = (int) image.getVoxel(x, y, z);
+                    if (label == 0)
+                        continue;
+
+                    // convert label to its index
+                    int index = labelIndices.get(label);
                     InertiaMoments3D moment = moments[index];
 
-    				// convert coordinates relative to centroid 
+                    // convert coordinates relative to centroid 
                     double x2 = x * sx - moment.cx;
                     double y2 = y * sy - moment.cy;
                     double z2 = z * sz - moment.cz;
 
-    				// update coefficients of inertia matrix
+                    // update coefficients of inertia matrix
                     moment.Ixx += x2 * x2;
                     moment.Iyy += y2 * y2;
                     moment.Izz += z2 * z2;
                     moment.Ixy += x2 * y2;
                     moment.Ixz += x2 * z2;
                     moment.Iyz += y2 * z2;
-    			}
-    		}
-    	}
+                }
+            }
+        }
 
-    	// normalize by number of pixels in each region 
-    	for (int i = 0; i < nLabels; i++) 
-    	{
+        // normalize by number of pixels in each region 
+        for (int i = 0; i < nLabels; i++) 
+        {
             moments[i].Ixx /= moments[i].count;
             moments[i].Iyy /= moments[i].count;
             moments[i].Izz /= moments[i].count;
             moments[i].Ixy /= moments[i].count;
             moments[i].Ixz /= moments[i].count;
             moments[i].Iyz /= moments[i].count;
-    	}
+        }
 
-    	// Create result array
-    	Ellipsoid[] ellipsoids = new Ellipsoid[nLabels];
-
-    	// compute ellipsoid parameters for each region
-        fireStatusChanged(this, "Ellipsoid: compute SVD");
-    	Matrix matrix = new Matrix(3, 3);
-    	for (int i = 0; i < nLabels; i++) 
-    	{
-            this.fireProgressChanged(this, i, nLabels);
-    		// fill up the 3x3 inertia matrix
-            InertiaMoments3D moment = moments[i];
-            matrix.set(0, 0, moment.Ixx);
-            matrix.set(0, 1, moment.Ixy);
-            matrix.set(0, 2, moment.Ixz);
-            matrix.set(1, 0, moment.Ixy);
-            matrix.set(1, 1, moment.Iyy);
-            matrix.set(1, 2, moment.Iyz);
-            matrix.set(2, 0, moment.Ixz);
-            matrix.set(2, 1, moment.Iyz);
-            matrix.set(2, 2, moment.Izz);
-
-    		// Extract singular values
-    		SingularValueDecomposition svd = new SingularValueDecomposition(matrix);
-    		Matrix values = svd.getS();
-
-    		// convert singular values to ellipsoid radii 
-    		double r1 = sqrt(5) * sqrt(values.get(0, 0));
-    		double r2 = sqrt(5) * sqrt(values.get(1, 1));
-    		double r3 = sqrt(5) * sqrt(values.get(2, 2));
-
-    		// extract |cos(theta)| 
-    		Matrix mat = svd.getU();
-    		double tmp = hypot(mat.get(1, 1), mat.get(2, 1));
-    		double phi, theta, psi;
-
-    		// avoid dividing by 0
-    		if (tmp > 16 * Double.MIN_VALUE) 
-    		{
-    			// normal case: theta <> 0
-    			psi     = atan2( mat.get(2, 1), mat.get(2, 2));
-    			theta   = atan2(-mat.get(2, 0), tmp);
-    			phi     = atan2( mat.get(1, 0), mat.get(0, 0));
-    		}
-    		else 
-    		{
-    			// theta is around 0 
-    			psi     = atan2(-mat.get(1, 2), mat.get(1,1));
-    			theta   = atan2(-mat.get(2, 0), tmp);
-    			phi     = 0;
-    		}
-
-    		// add coordinates of origin pixel (IJ coordinate system)
-    		double xc = moment.cx + .5 * sx + ox;
-    		double yc = moment.cy + .5 * sy + oy;
-    		double zc = moment.cz + .5 * sz + oz;
-    		
-    		// create the new ellipsoid
-    		ellipsoids[i] = new Ellipsoid(xc, yc, zc, r1, r2, r3, toDegrees(phi), toDegrees(theta), toDegrees(psi));
-    	}
-
-    	return ellipsoids;
+        // add coordinates of origin pixel (IJ coordinate system)
+        for (int i = 0; i < nLabels; i++) 
+        {
+            moments[i].cx += .5 * sx + ox;
+            moments[i].cy += .5 * sy + oy;
+            moments[i].cz += .5 * sz + oz;
+        }
+  
+        return moments;
 	}
-	
+
 	public class InertiaMoments3D
 	{
 	    // the number of voxels 
@@ -325,6 +294,53 @@ public class InertiaEllipsoid extends RegionAnalyzer3D<Ellipsoid>
         double Iyy = 0;
         double Iyz = 0;
         double Izz = 0;
+        
+        public Ellipsoid equivalentEllipsoid()
+        {
+            Matrix matrix = new Matrix(3, 3);
+            matrix.set(0, 0, this.Ixx);
+            matrix.set(0, 1, this.Ixy);
+            matrix.set(0, 2, this.Ixz);
+            matrix.set(1, 0, this.Ixy);
+            matrix.set(1, 1, this.Iyy);
+            matrix.set(1, 2, this.Iyz);
+            matrix.set(2, 0, this.Ixz);
+            matrix.set(2, 1, this.Iyz);
+            matrix.set(2, 2, this.Izz);
+
+            // Extract singular values
+            SingularValueDecomposition svd = new SingularValueDecomposition(matrix);
+            Matrix values = svd.getS();
+
+            // convert singular values to ellipsoid radii 
+            double r1 = sqrt(5) * sqrt(values.get(0, 0));
+            double r2 = sqrt(5) * sqrt(values.get(1, 1));
+            double r3 = sqrt(5) * sqrt(values.get(2, 2));
+
+            // extract |cos(theta)| 
+            Matrix mat = svd.getU();
+            double tmp = hypot(mat.get(1, 1), mat.get(2, 1));
+            double phi, theta, psi;
+
+            // avoid dividing by 0
+            if (tmp > 16 * Double.MIN_VALUE) 
+            {
+                // normal case: theta <> 0
+                psi     = atan2( mat.get(2, 1), mat.get(2, 2));
+                theta   = atan2(-mat.get(2, 0), tmp);
+                phi     = atan2( mat.get(1, 0), mat.get(0, 0));
+            }
+            else 
+            {
+                // theta is around 0 
+                psi     = atan2(-mat.get(1, 2), mat.get(1,1));
+                theta   = atan2(-mat.get(2, 0), tmp);
+                phi     = 0;
+            }
+
+            // create the new ellipsoid
+            return new Ellipsoid(this.cx, this.cy, this.cz, r1, r2, r3, toDegrees(phi), toDegrees(theta), toDegrees(psi));
+        }
 	}
 
 }
