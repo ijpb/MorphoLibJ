8734ef693c4f783f184d65ee465f79d3dd405bd0
Christian Tischer <christian.tischer@embl.de>
2020-11-17 15:41:19 +0100

    WIP Merge child and parent measurements

---
+++ src/main/java/inra/ijpb/measure/ResultsChildAndParentMerger.java
@@ -0,0 +1,128 @@
+package inra.ijpb.measure;
+
+import ij.measure.ResultsTable;
+
+import java.util.*;
+import java.util.stream.IntStream;
+
+public class ResultsChildAndParentMerger
+{
+	private final ResultsTable childTable;
+	private final String childName;
+	private final String childTableParentLabelColumn;
+	private final ResultsTable parentTable;
+	private final HashMap< Integer, Map< String, List< Double > > > parentToFeatureToMeasurements;
+	private final HashMap< Integer, Integer > parentLabelToRowIndex;
+
+	public enum AggregationMode
+	{
+		Mean,
+		Sum,
+		Max,
+		Min
+	}
+
+	public ResultsChildAndParentMerger( ResultsTable childTable, String childName, String parentLabelColumn, ResultsTable parentTable )
+	{
+		this.childTable = childTable;
+		this.childName = childName;
+		this.childTableParentLabelColumn = parentLabelColumn;
+		this.parentTable = parentTable;
+
+		parentLabelToRowIndex = initParentLabelToRowIndex();
+		parentToFeatureToMeasurements = initFeatureMap();
+		populateFeatureMap( parentToFeatureToMeasurements );
+	}
+
+	public ResultsTable appendToParentTable( AggregationMode mode )
+	{
+		parentToFeatureToMeasurements.keySet().stream().forEach( parentLabel ->
+		{
+			parentToFeatureToMeasurements.get( parentLabel ).keySet().stream().forEach( measurement ->
+			{
+				DoubleSummaryStatistics statistics = parentToFeatureToMeasurements.get( parentLabel ).get( measurement ).stream().mapToDouble( x -> x ).summaryStatistics();
+				Integer row = parentLabelToRowIndex.get( parentLabel );
+
+				if ( measurement.equals( "Label" ) )
+				{
+					final String column = childName + "_" + "Count";
+					parentTable.setValue( column, row, statistics.getCount() );
+				}
+				else
+				{
+					final String column = "" + childName + "_" + mode + "_" + measurement;
+
+					switch ( mode )
+					{
+						case Mean:
+							parentTable.setValue( column, row, statistics.getAverage() );
+						case Sum:
+							parentTable.setValue( column, row, statistics.getSum() );
+						case Max:
+							parentTable.setValue( column, row, statistics.getMax() );
+						case Min:
+							parentTable.setValue( column, row, statistics.getMin() );
+					}
+				}
+			} );
+		} );
+
+		return parentTable;
+	}
+
+	private HashMap< Integer, Map< String, List< Double > > > initFeatureMap()
+	{
+		HashMap< Integer, Map< String, List< Double > > > parentToFeatureToMeasurements = new HashMap<>();
+		IntStream.range( 0, parentTable.size() ).forEach( row ->
+		{
+			final HashMap< String, List< Double > > featureToMeasurements = new HashMap<>();
+			parentToFeatureToMeasurements.put( Integer.parseInt( parentTable.getLabel( row ) ), featureToMeasurements );
+			Arrays.stream( childTable.getHeadings() ).forEach( column -> featureToMeasurements.put( column, new ArrayList<>() ) );
+		} );
+
+		return parentToFeatureToMeasurements;
+	}
+
+	private void populateFeatureMap( HashMap< Integer, Map< String, List< Double > > > parentToFeatureToMeasurements )
+	{
+		Arrays.stream( childTable.getHeadings() ).forEach( column -> {
+			IntStream.range( 0, childTable.size() ).forEach( rowIndex ->
+			{
+				final int parentIndex = ( int ) childTable.getValue( childTableParentLabelColumn, rowIndex );
+
+				if ( parentIndex != 0 )
+				{
+					if ( column.equals( "Label" ) )
+					{
+						try
+						{
+							parentToFeatureToMeasurements.get( parentIndex ).get( column ).add( 1.0D );
+						} catch ( Exception e )
+						{
+							int a = 1;
+						}
+					} else
+					{
+						final double measurement = childTable.getValue( column, rowIndex );
+						parentToFeatureToMeasurements.get( parentIndex ).get( column ).add( measurement );
+					}
+				}
+				else
+				{
+					// child object that does not reside within any parent object
+				}
+			} );
+		} );
+	}
+
+	private HashMap< Integer, Integer > initParentLabelToRowIndex()
+	{
+		HashMap< Integer, Integer > parentLabelToRowIndex = new HashMap<>();
+		IntStream.range( 0, parentTable.size() ).forEach( row ->
+		{
+			parentLabelToRowIndex.put( Integer.parseInt( parentTable.getLabel( row ) ), row );
+		});
+
+		return parentLabelToRowIndex;
+	}
+}
